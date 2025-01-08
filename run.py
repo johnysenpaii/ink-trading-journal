c446ebe941bd02fbd7d0fa3fc1b750c4f2fa3b78
Johnny_Senpaii <johnjalosjos06@gmail.com>
2025-01-08 16:00:54 +0800

    waitres

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 # run.py
-
+from waitress import serve
 from app import create_app
 
 # Create the app instance
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0", port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=False, host="0.0.0.0", port=5000)
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=5000)
